6af93753ef39b94616a22d0a0fe94fe8a67fd5b8
bhayron <bhayronklivilan@gmail.com>
2021-07-02 10:08:30 -0300

    finalização posts

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,6 +4,7 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async create({ request }: HttpContextContract) {
     const { username, name } = request.only(['name', 'username'])
+    //console.log(username, name)
 
     const user = await User.create({
       username,
--- start/routes.ts
+++ start/routes.ts
@@ -7,8 +7,10 @@ Route.get('/', async () => {
 Route.get('/users', 'UsersController.index')
 Route.post('/users', 'UsersController.create')
 
-Route.get('/posts', 'PostsController.index')
-Route.post('/posts', 'PostsController.store')
-Route.get('/posts/:id', 'PostsController.show')
-Route.put('/posts/:id', 'PostsController.update')
-Route.delete('/posts/:id', 'PostsController.delete')
+// Route.get('/posts', 'PostsController.index')
+// Route.post('/posts', 'PostsController.store')
+// Route.get('/posts/:id', 'PostsController.show')
+// Route.put('/posts/:id', 'PostsController.update')
+// Route.delete('/posts/:id', 'PostsController.delete')
+
+Route.resource('/posts', 'PostsController').apiOnly()
